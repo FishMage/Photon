3c528bf5adb9cf7acd1d6c7288b218065690f6f2
FishMage <schen358@wisc.edu>
2017-10-03 14:17:47 -0400

    init Commit

---
+++ FirstPhoton/blinky.ino
@@ -0,0 +1,10 @@
+int led = D7;
+void setup(){
+    pinMode(led, OUTPUT);
+}
+void loop(){
+    digitalWrite(led, HIGH); //turn LED on
+    delay(1000); // wait 1 sec
+    digitalWrite(led, LOW); // turn LED off
+    delay(1000); // wait another second
+}
